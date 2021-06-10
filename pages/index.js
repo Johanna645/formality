762d2211d36e90e2d2b51034b817af0c1db9efce
Johanna Räihälä <johanna.raihala@gmail.com>
2021-06-10 10:53:28 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>This is the home page</h1>
+
+        <p className={styles.description}>
+          here will come the form with some buttons and input fields
+        </p>
+
+        <div className={styles.grid}>
+          <button>button</button>
+
+          <button>button</button>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  );
+}
